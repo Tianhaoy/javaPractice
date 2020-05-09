8146d7e34f72eb148377dbcfe13fe50e55f6af19
haoyu <2469653218@qq.com>
2020-05-09 22:12:24 +0800

    练习

---
+++ src/com/thy/nowCoder/Employee.java
@@ -0,0 +1,15 @@
+package com.thy.nowCoder;
+
+/**
+ * @Author 皓宇QAQ
+ * @email 2469653218@qq.com
+ * @Date 2020/5/5 15:05
+ * @link https://github.com/Tianhaoy/hanzomall
+ * @Description:
+ */
+public class Employee {
+    String empID = "0000";
+    public Employee(String id) {
+        empID = id;
+    }
+}
