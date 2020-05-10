4beb7bd0ff80a0978961ea8fa323b32e57525606
haoyu <2469653218@qq.com>
2020-05-10 19:07:04 +0800

    练习

--- src/com/thy/nowCoder/javaTest.java
+++ src/com/thy/nowCoder/javaTest.java
@@ -1,5 +1,10 @@
 package com.thy.nowCoder;
 
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @Author 皓宇QAQ
  * @email 2469653218@qq.com
@@ -8,30 +13,15 @@ package com.thy.nowCoder;
  * @Description:
  */
 public class javaTest {
-    class Super {
-        int flag = 1;
-
-        Super() {
-            test();
-        }
-
-        void test() {
-            System.out.println("Super.test() flag=" + flag);
-        }
-    }
-
-    class Sub extends Super {
-        Sub(int i) {
-            flag = i;
-            System.out.println("Sub.Sub()flag=" + flag);
-        }
-
-        void test() {
-            System.out.println("Sub.test()flag=" + flag);
-        }
-    }
-
+    long g= 0xfffL;
+    double d = 5.4545d;
+    long var;
+    public void javaTest(int param) { var = param; }//(1)
     public static void main(String[] args) {
-        new javaTest().new Sub(5);
+        javaTest a, b;
+        a =new javaTest();//(2)
+        b =new javaTest();//(3)
     }
+
 }
+
