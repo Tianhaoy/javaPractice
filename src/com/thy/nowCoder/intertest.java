269379a76ef7bb9ac3f30c2ddf84d946cbc435c9
haoyu <2469653218@qq.com>
2020-07-28 23:25:49 +0800

    test

---
+++ src/com/thy/nowCoder/intertest.java
@@ -0,0 +1,13 @@
+package com.thy.nowCoder;
+
+/**
+ * @Author 皓宇QAQ
+ * @email 2469653218@qq.com
+ * @Date 2020/5/23 18:34
+ * @link https://github.com/Tianhaoy/hanzomall
+ * @Description:
+ */
+public interface intertest {
+     abstract void  test();
+
+}
